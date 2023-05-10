c62a453766c2b6d91289d42838ee5a7ed2d30927
Péter Szilágyi <peterke@gmail.com>
2023-05-10 14:54:14 +0300

    crypto/kzg4844: pull in the C and Go libs for KZG cryptography (#27155)
    * cryto/kzg4844: pull in the C and Go libs for KZG cryptography
    
    * go.mod: pull in the KZG libraries
    
    * crypto/kzg4844: add basic becnhmarks for ballpark numbers
    
    * cmd, crypto: integrate both CKZG and GoKZG all the time, add flag
    
    * cmd/utils, crypto/kzg4844: run library init on startup
    
    * crypto/kzg4844: make linter happy
    
    * crypto/kzg4844: push missing file
    
    * crypto/kzg4844: fully disable CKZG but leave in the sources
    
    * build, crypto/kzg4844, internal: link CKZG by default and with portable mode
    
    * crypto/kzg4844: drop verifying the trusted setup in gokzg
    
    * internal/build: yolo until it works?
    
    * cmd/utils: make flag description friendlier
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>
    
    * crypto/ckzg: no need for double availability check
    
    * build: tiny flag cleanup nitpick
    
    ---------
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- build/ci.go
+++ build/ci.go
@@ -207,9 +207,9 @@ func doInstall(cmdline []string) {
 		csdb := build.MustLoadChecksums("build/checksums.txt")
 		tc.Root = build.DownloadGo(csdb, dlgoVersion)
 	}
-
-	// Disable CLI markdown doc generation in release builds.
-	buildTags := []string{"urfave_cli_no_docs"}
+	// Disable CLI markdown doc generation in release builds and enable linking
+	// the CKZG library since we can make it portable here.
+	buildTags := []string{"urfave_cli_no_docs", "ckzg"}
 
 	// Configure the build.
 	env := build.Env()
@@ -221,7 +221,6 @@ func doInstall(cmdline []string) {
 	if env.CI && runtime.GOARCH == "arm64" {
 		gobuild.Args = append(gobuild.Args, "-p", "1")
 	}
-
 	// We use -trimpath to avoid leaking local paths into the built executables.
 	gobuild.Args = append(gobuild.Args, "-trimpath")
 
@@ -301,7 +300,7 @@ func doTest(cmdline []string) {
 		csdb := build.MustLoadChecksums("build/checksums.txt")
 		tc.Root = build.DownloadGo(csdb, dlgoVersion)
 	}
-	gotest := tc.Go("test")
+	gotest := tc.Go("test", "-tags=ckzg")
 
 	// CI needs a bit more time for the statetests (default 10m).
 	gotest.Args = append(gotest.Args, "-timeout=20m")
