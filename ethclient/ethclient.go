49928ac6836ff8a45eaaf40385ed5ae8435b20b7
Péter Szilágyi <peterke@gmail.com>
2023-11-03 19:40:37 +0200

    ethclient: fix forwarding 1559 gas fields (#28462)

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -657,6 +657,12 @@ func toCallArg(msg ethereum.CallMsg) interface{} {
 	if msg.GasPrice != nil {
 		arg["gasPrice"] = (*hexutil.Big)(msg.GasPrice)
 	}
+	if msg.GasFeeCap != nil {
+		arg["maxFeePerGas"] = (*hexutil.Big)(msg.GasFeeCap)
+	}
+	if msg.GasTipCap != nil {
+		arg["maxPriorityFeePerGas"] = (*hexutil.Big)(msg.GasTipCap)
+	}
 	return arg
 }
 
