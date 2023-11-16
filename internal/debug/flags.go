4d7e6255d4d7c6f76d4e170c9155989648330819
zzycex <zzycex@gmail.com>
2023-11-16 14:14:49 +0800

    fix matric log

--- internal/debug/flags.go
+++ internal/debug/flags.go
@@ -149,6 +149,18 @@ func init() {
 }
 
 func setupLogHandler(ctx *cli.Context) (handler log.Handler) {
+	defer func() {
+		if !ctx.Bool(metricLogFlag.Name) {
+			inner := handler
+			handler = log.FuncHandler(func(r *log.Record) error {
+				if r.Msg == metricKey {
+					return nil
+				}
+
+				return inner.Log(r)
+			})
+		}
+	}()
 	var format log.Format
 	output := io.Writer(os.Stderr)
 	if ctx.Bool(logjsonFlag.Name) {
