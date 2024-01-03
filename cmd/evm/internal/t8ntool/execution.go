2cadd940d8b54b675b6a40b2e16ea354bdc9ed91
Mario Vega <marioevz@gmail.com>
2024-01-03 09:12:20 -0600

    cmd/evm: Fix blob-gas-used on invalid transactions in t8n (#28734)
    cmd/evm: fixes the blob gas calculation if a transaction is invalid

--- cmd/evm/internal/t8ntool/execution.go
+++ cmd/evm/internal/t8ntool/execution.go
@@ -129,6 +129,7 @@ func (pre *Prestate) Apply(vmConfig vm.Config, chainConfig *params.ChainConfig,
 		rejectedTxs []*rejectedTx
 		includedTxs types.Transactions
 		gasUsed     = uint64(0)
+		blobGasUsed = uint64(0)
 		receipts    = make(types.Receipts, 0)
 		txIndex     = 0
 	)
@@ -194,6 +195,7 @@ func (pre *Prestate) Apply(vmConfig vm.Config, chainConfig *params.ChainConfig,
 		if hashError != nil {
 			return nil, nil, NewError(ErrorMissingBlockhash, hashError)
 		}
+		blobGasUsed += txBlobGas
 		gasUsed += msgResult.UsedGas
 
 		// Receipt:
