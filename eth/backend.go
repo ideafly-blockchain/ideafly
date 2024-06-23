a931f12dbc323788e7a690c6a064bc2601fa8f18
sunnyjqs <sunnyjqshi180@nddn.io>
2024-06-23 15:21:02 +0800

    Delete comment

--- eth/backend.go
+++ eth/backend.go
@@ -223,14 +223,7 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 			Preimages:           config.Preimages,
 		}
 	)
-	//// Override the chain config with provided settings.
-	//var overrides core.ChainOverrides
-	//if config.OverrideTerminalTotalDifficulty != nil {
-	//	overrides.OverrideTerminalTotalDifficulty = config.OverrideTerminalTotalDifficulty
-	//}
-	//if config.OverrideTerminalTotalDifficultyPassed != nil {
-	//	overrides.OverrideTerminalTotalDifficultyPassed = config.OverrideTerminalTotalDifficultyPassed
-	//}
+
 	eth.blockchain, err = core.NewBlockChain(chainDb, cacheConfig, config.Genesis, &overrides, eth.engine, vmConfig, eth.shouldPreserve, &config.TxLookupLimit)
 	if err != nil {
 		return nil, err
