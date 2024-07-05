4da8834ecebac09bcc4354aeca078d267615784e
sunnyjqs <sunnyjqshi180@nddn.io>
2024-07-05 11:48:10 +0800

    fix bug: core/txpool: implement additional DoS defenses (#26648)

--- core/txpool/txpool.go
+++ core/txpool/txpool.go
@@ -755,7 +755,7 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (replaced bool, err e
 
 	// If the transaction pool is full, discard underpriced transactions
 	if uint64(pool.all.Slots()+numSlots(tx)) > pool.config.GlobalSlots+pool.config.GlobalQueue {
-		if replaced, err := pool.handleUnderpriced(tx, isLocal); err != nil {
+		if replaced, err := pool.handleUnderpriced(tx, isLocal, from, hash); err != nil {
 			return replaced, err
 		}
 	}
@@ -783,7 +783,7 @@ func (pool *TxPool) add(tx *types.Transaction, local bool) (replaced bool, err e
 	return replaced, nil
 }
 
-func (pool *TxPool) handleUnderpriced(tx *types.Transaction, isLocal bool) (replaced bool, err error) {
+func (pool *TxPool) handleUnderpriced(tx *types.Transaction, isLocal bool, from common.Address, hash common.Hash) (replaced bool, err error) {
 	// If the new transaction is underpriced, don't accept it
 	if !isLocal && pool.priced.Underpriced(tx) {
 		log.Trace("Discarding underpriced transaction", "hash", tx.Hash(), "gasTipCap", tx.GasTipCap(), "gasFeeCap", tx.GasFeeCap())
