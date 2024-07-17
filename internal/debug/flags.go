luxfi/sdk

internal/debug: add log.logfmt flag to set logging to use logfmt (#26970)

internal/debug/flags.go
@@ -54,6 +54,11 @@ var (
 		Usage:    "Format logs with JSON",
 		Category: flags.LoggingCategory,
 	}
+	logfmtFlag = &cli.BoolFlag{
+		Name:     "log.logfmt",
+		Usage:    "Format logs with logfmt",
+		Category: flags.LoggingCategory,
+	}
 	logFileFlag = &cli.StringFlag{
 		Name:     "log.file",
 		Usage:    "Write logs to a file",
@@ -115,6 +120,7 @@ var Flags = []cli.Flag{
 	verbosityFlag,
 	vmoduleFlag,
 	logjsonFlag,
+	logfmtFlag,
 	logFileFlag,
 	backtraceAtFlag,
 	debugFlag,
@@ -147,6 +153,8 @@ func Setup(ctx *cli.Context) error {
 	var logfmt log.Format
 	if ctx.Bool(logjsonFlag.Name) {
 		logfmt = log.JSONFormat()
+	} else if ctx.Bool(logfmtFlag.Name) {
+		logfmt = log.LogfmtFormat()
 	} else {
 		logfmt = log.TerminalFormat(useColor)
 	}

ideafly-blockchain/ideafly

internal/debug/flags.go
@@ -66,6 +66,11 @@ var (
 		Usage:    "Format logs with JSON",
 		Category: flags.LoggingCategory,
 	}
+	logfmtFlag = &cli.BoolFlag{
+		Name:     "log.logfmt",
+		Usage:    "Format logs with logfmt",
+		Category: flags.LoggingCategory,
+	}
 	backtraceAtFlag = &cli.StringFlag{
 		Name:     "log.backtrace",
 		Usage:    "Request a stack trace at a specific logging statement (e.g. \"block.go:271\")",
@@ -124,6 +129,7 @@ var Flags = []cli.Flag{
 	metricLogFlag,
 	vmoduleFlag,
 	logjsonFlag,
+	logfmtFlag,
 	backtraceAtFlag,
 	debugFlag,
 	pprofFlag,
@@ -165,6 +171,8 @@ func setupLogHandler(ctx *cli.Context) (handler log.Handler) {
 	output := io.Writer(os.Stderr)
 	if ctx.Bool(logjsonFlag.Name) {
 		format = log.JSONFormat()
+	} 	else if ctx.Bool(logfmtFlag.Name) {
+		format = log.LogfmtFormat()
 	} else {
 		usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
 		if usecolor {