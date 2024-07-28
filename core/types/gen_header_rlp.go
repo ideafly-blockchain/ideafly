luxfi/sdk

core/types:  go generate (#27196)
Fixes a discrepancy between source and generated files, which was introduced when ExcessDataGas was added in https://github.com/ethereum/go-ethereum/pull/27046.

core/types/gen_header_rlp.go
@@ -42,7 +42,8 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 	w.WriteBytes(obj.Nonce[:])
 	_tmp1 := obj.BaseFee != nil
 	_tmp2 := obj.WithdrawalsHash != nil
-	if _tmp1 || _tmp2 {
+	_tmp3 := obj.ExcessDataGas != nil
+	if _tmp1 || _tmp2 || _tmp3 {
 		if obj.BaseFee == nil {
 			w.Write(rlp.EmptyString)
 		} else {
@@ -52,11 +53,21 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 			w.WriteBigInt(obj.BaseFee)
 		}
 	}
-	if _tmp2 {
+	if _tmp2 || _tmp3 {
 		if obj.WithdrawalsHash == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteBytes(obj.WithdrawalsHash[:])
+		}
+	}
+	if _tmp3 {
+		if obj.ExcessDataGas == nil {
+			w.Write(rlp.EmptyString)
+		} else {
+			if obj.ExcessDataGas.Sign() == -1 {
+				return rlp.ErrNegativeBigInt
+			}
+			w.WriteBigInt(obj.ExcessDataGas)
 		}
 	}
 	w.ListEnd(_tmp0)

ideafly-blockchain/ideafly

core/types/gen_header_rlp.go
@@ -44,7 +44,8 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 	w.WriteBytes(obj.MixDigest[:])
 	w.WriteBytes(obj.Nonce[:])
 	_tmp1 := obj.BaseFee != nil
-	if _tmp1 {
+	_tmp3 := obj.ExcessDataGas != nil
+	if _tmp1||_tmp3 {
 		if obj.BaseFee == nil {
 			w.Write(rlp.EmptyString)
 		} else {
@@ -52,6 +53,16 @@ func (obj *Header) EncodeRLP(_w io.Writer) error {
 				return rlp.ErrNegativeBigInt
 			}
 			w.WriteBigInt(obj.BaseFee)
+		}
+	}
+	if _tmp3 {
+		if obj.ExcessDataGas == nil {
+			w.Write(rlp.EmptyString)
+		} else {
+			if obj.ExcessDataGas.Sign() == -1 {
+				return rlp.ErrNegativeBigInt
+			}
+			w.WriteBigInt(obj.ExcessDataGas)
 		}
 	}
 	w.ListEnd(_tmp0)