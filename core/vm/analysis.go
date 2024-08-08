2e46419061947150f34ef1d63e2f09cfe2f9d729
lingtei <956337729@qq.com>
2024-08-08 11:09:25 +0800

    make lint

--- core/vm/analysis.go
+++ core/vm/analysis.go
@@ -25,8 +25,6 @@ const (
 	set7BitsMask = uint16(0b111_1111)
 )
 
-const codeBitmapCacheSize = 10000
-
 // bitvec is a bit vector which maps bytes in a program.
 // An unset bit means the byte is an opcode, a set bit means
 // it's data (i.e. argument of PUSHxx).
